f7233906aa67500541a2f66fe71a6c8670c9308d
Iankaikkinen <nicklevkovic@gmail.com>
2018-09-11 04:05:47 +0300

    20/80 are working. Need array position fix

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var uniqueItems = Array.from(new Set(preferences));
+  return (uniqueItems.length/3)>>0;
 };
