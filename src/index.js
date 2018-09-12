de477570c238d8c677d16e36655b63a8a26eefa7
Iankaikkinen <nicklevkovic@gmail.com>
2018-09-12 12:34:28 +0300

    Final solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param a - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(a= []) {
   // your implementation
-  var uniqueItems = Array.from(new Set(preferences));
-  return (uniqueItems.length/3)>>0;
+ let counter = 0;  
+ for (i = 0; i<(a.length); i++) { 
+     var lover1 = a[i] - 1;       if (lover1 == i) continue;
+     var lover2 = a[lover1] - 1;  if (lover1 == lover2) continue;
+     var lover3 = a[lover2] - 1;  if (lover3 == lover2) continue;
+     if (lover3 == i) { counter++; }
+  }
+return Math.floor(counter / 3);
 };
